abd9156e78d6c42b9d04d5c6380672a55644664e
Omar Espana <omi.fullstack@gmail.com>
2025-06-17 16:34:29 +0100

    Init migration for nvim configuration

---
+++ init.lua
@@ -0,0 +1,71 @@
+--- Nvim Init cingofiguration file
+
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+vim.g.have_nerd_font = true
+
+-- [[ Setting options ]]
+vim.opt.colorcolumn = '80' -- Add vertical line at column 80
+-- Make line numbers default
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.guifont = 'JetBrainsMono Nerd Font'
+
+vim.opt.statuscolumn = '%s %=%{v:relnum?v:relnum:v:lnum} '
+vim.opt.signcolumn = 'auto'
+vim.opt.foldcolumn = 'auto'
+
+vim.opt.debug = {
+  on = true, -- or false
+  -- other debug options here if needed
+}
+-- Enable mouse mode, can be useful for resizing splits for example!
+vim.opt.mouse = 'a'
+
+-- Don't show the mode, since it's already in the status line
+vim.opt.showmode = false
+
+-- Sync clipboard
+vim.opt.clipboard = 'unnamedplus'
+
+-- Enable break indent
+vim.opt.breakindent = true
+
+-- Save undo history
+vim.opt.undofile = true
+
+-- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- Decrease update time
+vim.opt.updatetime = 250
+
+-- Decrease mapped sequence wait time
+-- Displays which-key popup sooner
+vim.opt.timeoutlen = 300
+
+-- Configure how new splits should be opened
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+-- Sets how neovim will display certain whitespace characters in the editor.
+vim.opt.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
+-- Preview substitutions live, as you type!
+vim.opt.inccommand = 'split'
+
+-- Show which line your cursor is on
+vim.opt.cursorline = true
+
+-- Minimal number of screen lines to keep above and below the cursor.
+vim.opt.scrolloff = 10
+
+-- Set highlight on search, but clear on pressing <Esc> in normal mode
+vim.opt.hlsearch = true
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+
