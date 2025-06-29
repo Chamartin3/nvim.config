46cf869517a9b784b306c289da523092695a3301
Omar Espana <omi.fullstack@gmail.com>
2025-06-29 16:45:13 +0100

    Migrtationg configuration for noice and autosessions

---
+++ lua/plugins/sessions.lua
@@ -0,0 +1,55 @@
+return {
+  'rmagatti/auto-session',
+  config = function()
+    local auto_session = require 'auto-session'
+    auto_session.setup {
+      auto_restore_enabled = true,
+      auto_session_use_git_branch = true,
+      git_auto_restore_on_branch_change = true,
+      -- auto_create = function()
+      --   local cmd = '"[[$(git rev-parse --show-toplevel)" == "$(pwd)" ]] && echo "true" || echo "false"'
+      --   return vim.fn.system(cmd) == 'true\n'
+      -- end,
+      auto_session_suppress_dirs = { '~/', '~/Downloads', '~/Documents', '~/Desktop/' },
+      session_lens = {
+        buftypes_to_ignore = {},
+        load_on_setup = true,
+        theme_conf = { border = true },
+        previewer = false,
+      },
+      save_extra_cmds = {
+        function()
+          -- TODO: Add hooks for git checkouts
+          local qflist = vim.fn.getqflist()
+          -- return nil to clear any old qflist
+          if #qflist == 0 then
+            return nil
+          end
+          local qfinfo = vim.fn.getqflist { title = 1 }
+
+          for _, entry in ipairs(qflist) do
+            -- use filename instead of bufnr so it can be reloaded
+            entry.filename = vim.api.nvim_buf_get_name(entry.bufnr)
+            entry.bufnr = nil
+          end
+
+          local setqflist = 'call setqflist(' .. vim.fn.string(qflist) .. ')'
+          local setqfinfo = 'call setqflist([], "a", ' .. vim.fn.string(qfinfo) .. ')'
+          return { setqflist, setqfinfo, 'copen' }
+        end,
+      },
+    }
+
+    local keymap = vim.keymap
+    vim.keymap.set('n', '<leader>ss', require('auto-session.session-lens').search_session, {
+      noremap = true,
+      desc = 'Search Session',
+    })
+    keymap.set('n', '<leader>wr', '<cmd>SessionRestore<CR>', {
+      desc = 'Restore session for cwd',
+    })
+    keymap.set('n', '<leader>ws', '<cmd>SessionSave<CR>', {
+      desc = 'Save session for auto session root dir',
+    })
+  end,
+}
--- lua/plugins/theming.lua
+++ lua/plugins/theming.lua
@@ -142,3 +142,132 @@ return {
       }
     end,
   },
+  {
+    --- NOICE.NVIM
+    --  Pretty command line UI
+    -- TODO: Explore nvim command line configuration in snacks
+    'folke/noice.nvim',
+    event = 'VeryLazy',
+    dependencies = {
+      'MunifTanjim/nui.nvim',
+      'rcarriga/nvim-notify',
+    },
+    config = function()
+      require('lualine').setup {
+        sections = {
+          lualine_x = {
+            {
+              require('noice').api.status.message.get_hl,
+              cond = require('noice').api.status.message.has,
+            },
+            {
+              require('noice').api.status.command.get,
+              cond = require('noice').api.status.command.has,
+              color = { fg = '#ff9e64' },
+            },
+            {
+              require('noice').api.status.mode.get,
+              cond = require('noice').api.status.mode.has,
+              color = { fg = '#ff9e64' },
+            },
+            {
+              require('noice').api.status.search.get,
+              cond = require('noice').api.status.search.has,
+              color = { fg = '#ff9e64' },
+            },
+          },
+        },
+      }
+      require('telescope').load_extension 'noice'
+      local wk = require 'which-key'
+      wk.add({
+        { '<leader>n', group = '[N]otifications', icon = '󱥁 ' },
+        { '<leader>ns', '<cmd>NoiceTelescope<CR>', desc = '[N]otification Search' },
+        { '<leader>nd', '<cmd>NoiceDismiss<CR>', desc = 'Dismiss' },
+        { '<leader>nh', '<cmd>NoiceHistory<CR>', desc = 'Show [H]istory' },
+        { '<leader>nl', '<cmd>NoiceLast<CR>', desc = 'Show [L]ast' },
+        { '<leader>na', '<cmd>NoiceAll<CR>', desc = '[A]ll Messages in a buffer' },
+      }, { mode = 'n' })
+
+      -- Print result from command line
+      vim.keymap.set('c', '<S-Enter>', function()
+        require('noice').redirect(vim.fn.getcmdline())
+      end, { desc = 'Redirect Cmdline' })
+
+      --- Navigate form lsp messages
+      vim.keymap.set({ 'n', 'i', 's' }, '<c-f>', function()
+        if not require('noice.lsp').scroll(4) then
+          return '<c-f>'
+        end
+      end, { silent = true, expr = true })
+
+      vim.keymap.set({ 'n', 'i', 's' }, '<c-b>', function()
+        if not require('noice.lsp').scroll(-4) then
+          return '<c-b>'
+        end
+      end, { silent = true, expr = true })
+      local lsp_settings = {
+        override = {
+          ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+          ['vim.lsp.util.stylize_markdown'] = true,
+          ['cmp.entry.get_documentation'] = true, -- requires hrsh7th/nvim-cmp
+        },
+      }
+      require('noice').setup {
+        lsp = lsp_settings,
+        preset = {
+          command_palette = true,
+          long_message_to_split = true,
+        },
+        health = {
+          checker = false, -- Disable if you don't want health checks to run
+        },
+        all = {
+          view = 'popup',
+          opts = { enter = true, format = 'details' },
+          filter = {},
+        },
+        -- cmdline = {
+        --   enabled = true,
+        --   view = 'cmdline_popup',
+        --   opts = {},
+        --   format = {
+        --     cmdline = { pattern = '^:', icon = '', lang = 'vim' },
+        --     search_down = { kind = 'search', pattern = '^/', icon = ' ', lang = 'regex' },
+        --     search_up = { kind = 'search', pattern = '^%?', icon = ' ', lang = 'regex' },
+        --     filter = { pattern = '^:%s*!', icon = '$', lang = 'bash' },
+        --     lua = { pattern = { '^:%s*lua%s+', '^:%s*lua%s*=%s*', '^:%s*=%s*' }, icon = '', lang = 'lua' },
+        --     help = { pattern = '^:%s*he?l?p?%s+', icon = '' },
+        --     input = { view = 'cmdline_input', icon = '󰥻 ' },
+        --   },
+        -- },
+      }
+    end,
+  },
+  {
+    'folke/snacks.nvim',
+    priority = 1000,
+    lazy = false,
+    ---@type snacks.Config
+    opts = {
+      -- dashboard = { enabled = true },
+      -- explorer = { enabled = true },
+      indent = { enabled = true },
+      input = {
+        enabled = true,
+        win = { style = 'input' },
+        icon = ' ',
+        icon_hl = 'SnacksInputIcon',
+        icon_pos = 'left',
+        prompt_pos = 'title',
+      },
+      picker = { enabled = true },
+      -- notifier = { enabled = true },
+      quickfile = { enabled = true },
+      scope = { enabled = true },
+      scroll = { enabled = true },
+      statuscolumn = { enabled = true },
+      -- words = { enabled = true },
+    },
+  },
+}
