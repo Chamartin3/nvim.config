bf9254b91bf8626f47b196c92b703550b714bdf4
Omar Espana <omi.fullstack@gmail.com>
2025-12-28 10:15:22 +0100

    Remove hover an add nvim sourround

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -93,8 +93,27 @@ return {
       },
     },
   },
-  -- TODO: Try out
-  -- {"tpope/vim-surround"},
+
+  {
+    'kylechui/nvim-surround',
+    version = '*',
+    event = 'VeryLazy',
+    opts = {
+      highlight = { duration = 500 },
+    },
+    init = function()
+      local wk = require 'which-key'
+      wk.add({
+        { 'cs', desc = 'Change surround' },
+        { 'ds', desc = 'Delete surround' },
+        { 'ys', desc = 'Add surround' },
+        { 'yss', desc = 'Surround line' },
+      }, { mode = 'n' })
+      wk.add({
+        { 'S', desc = 'Surround selection' },
+      }, { mode = 'x' })
+    end,
+  },
   {
     'rachartier/tiny-inline-diagnostic.nvim',
     event = 'VeryLazy',
@@ -107,34 +126,24 @@ return {
       }
     end,
   },
-  -- BUG: Not working properly
-  -- {
-  --   'Bekaboo/deadcolumn.nvim',
-  --   config = function()
-  --     local opts = {
-  --       scope = 'line', ---@type string|fun(): integer
-  --       ---@type string[]|fun(mode: string): boolean
-  --       modes = function(mode)
-  --         return mode:find '^[ictRss\x13]' ~= nil
-  --       end,
-  --       blending = {
-  --         threshold = 0.75,
-  --         colorcode = '#000000',
-  --         hlgroup = { 'Normal', 'bg' },
-  --       },
-  --       warning = {
-  --         alpha = 0.4,
-  --         offset = 0,
-  --         colorcode = '#FF0000',
-  --         hlgroup = { 'Error', 'bg' },
-  --       },
-  --       extra = {
-  --         ---@type string?
-  --         follow_tw = nil,
-  --       },
-  --     }
-  --
-  --     require('deadcolumn').setup(opts)
-  --   end,
-  -- },
+  {
+    'Bekaboo/deadcolumn.nvim',
+    opts = {
+      scope = 'line',
+      modes = function()
+        return true
+      end,
+      blending = {
+        threshold = 0.75,
+        colorcode = '#000000',
+        hlgroup = { 'Normal', 'bg' },
+      },
+      warning = {
+        alpha = 0.4,
+        offset = 0,
+        colorcode = '#FF0000',
+        hlgroup = { 'Error', 'bg' },
+      },
+    },
+  },
 }
